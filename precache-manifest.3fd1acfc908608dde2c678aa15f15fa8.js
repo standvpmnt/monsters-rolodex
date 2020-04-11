db9ae3a0b495768c7ca9fa9dd8538711f7a836b7
STan Dvpmnt <standvpmnt@gmail.com>
2020-04-11 13:57:50 +0530

    Updates

---
+++ precache-manifest.3fd1acfc908608dde2c678aa15f15fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a9bdd1ab4e1e315ecdca4fcce8ec0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c74e4f5bd59c7dfded9c",
+    "url": "/monsters-rolodex/static/css/main.0ca875f2.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c74e4f5bd59c7dfded9c",
+    "url": "/monsters-rolodex/static/js/main.19eb2d4b.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
